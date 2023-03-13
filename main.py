74f053e7252635b2fc0da080edf00cc5557e5858
adian12 <69429107+adian12@users.noreply.github.com>
2023-03-12 20:30:20 -0700

    Added the option to raise g to a factorial

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import math
+
 def mod(g,exponent, p):
     return pow(g,exponent,p)
 
 def main():
     g = int(input("Enter the base g? g^e (mod P) "))
-    e = int(input("Enter exponent e? g^e (mod p) "))
+
+    factorial = input("Does the exponent include a factorial?")
+    if factorial:
+        e = int(input("Enter the exponent e without the factorial expression. g^e! (mod p)"))
+        temp = math.factorial(e)
+        e = temp
+    else:
+        e = int(input("Enter exponent e? g^e (mod p) "))
+    
     p = int(input("Enter the modulus p: g^e (mod p) "))
+    
 
     print(mod(g,e,p))
     
